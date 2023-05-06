a456962a540f0559a3ccc97f2a846fd1e6cfdd0c
Parth Gupta <parthgupta3003@gmail.com>
2023-05-06 16:37:58 +0530

    added grid mode

---
+++ tkinterwin.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+def display_grid():
+    global rootgrid
+    rootgrid = tk.Tk()
+    rootgrid.overrideredirect(True)
+    rootgrid.geometry("{0}x{1}+0+0".format(rootgrid.winfo_screenwidth(), rootgrid.winfo_screenheight()))
+    rootgrid.resizable(False, False)
+    rootgrid.attributes("-alpha", 0.3)
+
+    width = rootgrid.winfo_screenwidth() // 10
+    height = rootgrid.winfo_screenheight() // 7
+
+    for i in range(10):
+        for j in range(7):
+            num = j * 10 + i + 1  # calculate the block number
+            block = tk.Frame(rootgrid, width=width, height=height, bg="white")
+            label = tk.Label(block, text=num, font=("Helvetica", 16), fg="black", bg="white")
+            label.place(relx=0.5, rely=0.5, anchor="center")
+            block.grid(row=j, column=i, padx=0, pady=0)
+
+    rootgrid.mainloop()
+
+def destroy_win():
+    global rootgrid
+    rootgrid.destroy()
